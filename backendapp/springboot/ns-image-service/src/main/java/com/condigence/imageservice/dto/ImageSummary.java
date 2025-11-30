c8803db17df259b058833df2e31e938be18ceac6
condigence <condigence@gmail.com>
2025-11-30 16:22:44 +0530

    added lombok

--- backendapp/springboot/ns-image-service/src/main/java/com/condigence/imageservice/dto/ImageSummary.java
+++ backendapp/springboot/ns-image-service/src/main/java/com/condigence/imageservice/dto/ImageSummary.java
@@ -1,5 +1,12 @@
 package com.condigence.imageservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageSummary {
     private Long id;
     private String name;
@@ -7,34 +14,4 @@ public class ImageSummary {
     private Long imageSize;
     private String moduleName;
     private String type;
-
-    public ImageSummary() {}
-
-    public ImageSummary(Long id, String name, String imageName, Long imageSize, String moduleName, String type) {
-        this.id = id;
-        this.name = name;
-        this.imageName = imageName;
-        this.imageSize = imageSize;
-        this.moduleName = moduleName;
-        this.type = type;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getImageName() { return imageName; }
-    public void setImageName(String imageName) { this.imageName = imageName; }
-
-    public Long getImageSize() { return imageSize; }
-    public void setImageSize(Long imageSize) { this.imageSize = imageSize; }
-
-    public String getModuleName() { return moduleName; }
-    public void setModuleName(String moduleName) { this.moduleName = moduleName; }
-
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
 }
-
--- backendapp/springboot/ns-image-service/src/main/java/com/condigence/imageservice/entity/Image.java
+++ backendapp/springboot/ns-image-service/src/main/java/com/condigence/imageservice/entity/Image.java
@@ -5,9 +5,21 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "nsimage")
 @SuppressWarnings("unused")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Image implements Serializable {
 
 	@Serial
@@ -17,93 +29,28 @@ public class Image implements Serializable {
 	@Column(name = "id")
 	@SequenceGenerator(name = "hibernate_sequence", sequenceName = "hibernate_sequence", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_sequence")
+	@EqualsAndHashCode.Include
+	@ToString.Include
 	private Long id;
 
 	@Column(name = "module_name")
 	private String moduleName;
-	
-	public String getModuleName() {
-		return moduleName;
-	}
-
-	public void setModuleName(String moduleName) {
-		this.moduleName = moduleName;
-	}
 
 	@Column(name = "name")
+	@ToString.Include
 	private String name;
 
 	@Column(name = "type")
+	@JsonProperty("type")
 	private String type;
 
 	@Column(name = "size")
+	@JsonProperty("imageSize")
 	private Long imageSize;
 
 	@Column(name = "unique_name")
 	private String imageName;
 
-	public Image() {
-		// JPA requires a no-arg constructor
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@JsonProperty("type")
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@JsonProperty("imageSize")
-	public Long getImageSize() {
-		return imageSize;
-	}
-
-	public void setImageSize(Long imageSize) {
-		this.imageSize = imageSize;
-	}
-
-	public String getImageName() {
-		return imageName;
-	}
-
-	public void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-
-	@Override
-	public String toString() {
-		return "Image [id=" + id + ", moduleName=" + moduleName + ", name=" + name + ", type=" + type + ", imageSize=" + imageSize + ", imageName=" + imageName + "]";
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Image image = (Image) o;
-		return id != null && id.equals(image.id);
-	}
-
-	@Override
-	public int hashCode() {
-		return id == null ? 0 : id.hashCode();
-	}
+	// JPA requires a no-arg constructor; Lombok provides a protected one above
 
 }
